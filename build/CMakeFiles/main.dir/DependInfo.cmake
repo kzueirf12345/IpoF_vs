f15fcdbddcd495eb48c7467ff9698fa4bebeec13
kzueirf12345 <jaba0192837465@gmail.com>
2024-01-29 21:23:25 +0300

    last version with pointers

--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -11,9 +11,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/urodish/programming/IpoF_vs/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/urodish/programming/IpoF_vs/build/classes/CMakeFiles/cls.dir/DependInfo.cmake"
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/classes/CMakeFiles/cls.dir/DependInfo.cmake
+++ build/classes/CMakeFiles/cls.dir/DependInfo.cmake
@@ -15,8 +15,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/urodish/programming/IpoF_vs/classes/Crystal.cpp" "classes/CMakeFiles/cls.dir/Crystal.cpp.o" "gcc" "classes/CMakeFiles/cls.dir/Crystal.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
